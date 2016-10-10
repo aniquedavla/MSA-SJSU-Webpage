bbe3a828082475e1ac77cd5b594ba3efc6584e8e
Anique <aniquedavla@Aniques-MacBook-Pro.local>
2016-10-09 18:46:04 -0700

    mend

---
+++ msaWebpage/build/gulpfile.js
@@ -0,0 +1,26 @@
+svar gulp = require('gulp');
+
+gulp.task('default', function() {
+  // place code for your default task here
+  gulp.src('msasjsu.html');
+});
+
+0.0.0.0         putlocker.is
+0.0.0.0         www.hdmovie14.net
+151.101.40.133  www.putlocker.is
+151.101.40.133  megahd.org
+151.101.40.133  www.megahd.org
+0.0.0.0         www.movienest.co
+0.0.0.0         movienest.co
+0.0.0.0         https://www.megahd.org
+0.0.0.0         www.megahd.org
+0.0.0.0         www.megashare9.online
+0.0.0.0          www.hdmovie14.net
+0.0.0.0          www.movierulz.is
+0.0.0.0         movierulz.is
+0.0.0.0         megashare9.online
+0.0.0.0         hdmovie14.net
+0.0.0.0         megahd.org
+www.youtube.com facebook.com
+
+
